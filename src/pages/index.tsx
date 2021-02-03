b97fcb4b220b8a3c59478d9e76490f702dcacbde
jacobmiller22 <jacobmiller22@vt.edu>
2021-02-02 21:51:58 -0500

    Transitions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import Head from "next/head";
 
-import "../../node_modules/bootstrap/dist/css/bootstrap.css";
-
 import IntroView from "../Views/IntroView";
 import ProjectsView from "../Views/ProjectsView";
 import AboutView from "../Views/AboutView";
 import FooterView from "../Views/FooterView";
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const contactInfo = {
@@ -36,16 +39,24 @@ export default function Home() {
   };
 
   return (
-    <Container className='container'>
+    <>
       <Head>
         <title>Jacob Miller</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Col>
+        <Row>
+          <IntroView />
+        </Row>
+        <Row className={styles.alt}>
+          <ProjectsView />
+        </Row>
+        <Row className={styles.primary}>
+          <AboutView contactInfo={contactInfo} />
+        </Row>
 
-      <IntroView />
-      <ProjectsView />
-      <AboutView contactInfo={contactInfo} />
-      <FooterView contactInfo={contactInfo} />
-    </Container>
+        <FooterView contactInfo={contactInfo} />
+      </Col>
+    </>
   );
 }
